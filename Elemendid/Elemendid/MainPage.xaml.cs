58e8ee0ab5a79a5af5d668ba8f66df941b00132d
opilane <opilane@HP-CZC6297BBL>
2020-10-23 09:54:47 +0300

    navigation

--- Elemendid/Elemendid/MainPage.xaml.cs
+++ Elemendid/Elemendid/MainPage.xaml.cs
@@ -8,76 +8,100 @@ using Xamarin.Forms;
 
 namespace Elemendid
 {
+    
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
-            InitializeComponent();
-            Button Tbl_btn = new Button { Text = "Table Page" };
+            BackgroundColor = Color.White; //
+            Button tblp_btn = new Button { Text = "TabelPage", 
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5, Padding = 10 };
             StackLayout st = new StackLayout();
-            st.Children.Add(Tbl_btn);
+            st.Children.Add(tblp_btn);
             Content = st;
-            Tbl_btn.Clicked += Tbl_btn_Clicked;
+            tblp_btn.Clicked += tblp_btn_Clicked;
 
-            InitializeComponent();
-            Button Tbl_btn2 = new Button { Text = "Soonputru" };
-            StackLayout st2 = new StackLayout();
-            st2.Children.Add(Tbl_btn2);
-            Content = st2;
-            Tbl_btn2.Clicked += Tbl_btn2_Clicked;
+            Button jalutan_btn = new Button { Text = "jalutan",
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5 };
+            st.Children.Add(jalutan_btn);
+            Content = st;
+            jalutan_btn.Clicked += jalutan_btn_Clicked;
 
-            InitializeComponent();
-            Button Tbl_btn3 = new Button { Text = "Jalutan" };
-            StackLayout st3 = new StackLayout();
-            st3.Children.Add(Tbl_btn3);
-            Content = st3;
-            Tbl_btn3.Clicked += Tbl_btn3_Clicked;
+            Button Lahenlounale_btn = new Button { Text = "Lähen lõunale", 
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5 };
+            st.Children.Add(Lahenlounale_btn);
+            Content = st;
+            Lahenlounale_btn.Clicked += Lahenlounale_btn_Clicked;
 
-            InitializeComponent();
-            Button Tbl_btn4 = new Button { Text = "Tegelen spordiga" };
-            StackLayout st4 = new StackLayout();
-            st4.Children.Add(Tbl_btn4);
-            Content = st4;
-            Tbl_btn4.Clicked += Tbl_btn4_Clicked;
+            Button Magan_btn = new Button { Text = "Magan", 
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5 };
+            st.Children.Add(Magan_btn);
+            Content = st;
+            Magan_btn.Clicked += Magan_btn_Clicked;
 
-            InitializeComponent();
-            Button Tbl_btn5 = new Button { Text = "Söön" };
-            StackLayout st5 = new StackLayout();
-            st5.Children.Add(Tbl_btn5);
-            Content = st5;
-            Tbl_btn5.Clicked += Tbl_btn5_Clicked;
+            Button Soon_btn = new Button { Text = "Söön",
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5 };
+            st.Children.Add(Soon_btn);
+            Content = st;
+            Soon_btn.Clicked += Soon_btn_Clicked;
 
+            Button Soonputru_btn = new Button { Text = "Söön putru", 
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5 };
+            st.Children.Add(Soonputru_btn);
+            Content = st;
+            Soonputru_btn.Clicked += Soonputru_btn_Clicked;
 
+            Button Tegelenspordiga_btn = new Button { Text = "Tegelen spordiga",
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5 };
+            st.Children.Add(Tegelenspordiga_btn);
+            Content = st;
+            Tegelenspordiga_btn.Clicked += Tegelenspordiga_btn_Clicked;
 
+            Button Tousenpusti_btn = new Button { Text = "Tõusen püsti", 
+            BackgroundColor = Color.FromRgb(0, 149, 248), Margin = 5 };
+            st.Children.Add(Tousenpusti_btn);
+            Content = st;
+            Tousenpusti_btn.Clicked += Tousenpusti_btn_Clicked;
         }
 
-        private async void Tbl_btn5_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new Soon());
-        }
-
-        private async void Tbl_btn4_Clicked(object sender, EventArgs e)
+        private async void Tegelenspordiga_btn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Tegelenspordiga());
         }
 
-        private async void Tbl_btn3_Clicked(object sender, EventArgs e)
+        private async void Magan_btn_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new Jalutan());
+            await Navigation.PushAsync(new Magan());
         }
 
-        private async void Tbl_btn2_Clicked(object sender, EventArgs e)
+        private async void Tousenpusti_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Tousenpusti());
+        }
+
+        private async void Soonputru_btn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Soonputru());
         }
 
-        private async void Tbl_btn_Clicked(object sender, EventArgs e)
+        private async void jalutan_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Jalutan());
+        }
+
+        private async void Lahenlounale_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Lahenlounale());
+        }
+
+        private async void Soon_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Soon());
+        }
+
+        private async void tblp_btn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new TabelPage());
         }
-        
-        
-        
-
     }
 }
